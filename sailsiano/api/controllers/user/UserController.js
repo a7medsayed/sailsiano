7b741c78a8fc4422f550c524e2f36f294a1df517
ahmed sayed <ahmedsayedmohammed93@gmail.com>
2022-12-10 15:27:00 +0200

    define user resources and routes

--- sailsiano/api/controllers/user/UserController.js
+++ sailsiano/api/controllers/user/UserController.js
@@ -1,3 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
 /**
  * UserController
  *
@@ -7,6 +11,54 @@
 
 module.exports = {
   
+    async registerNewUser(req, res) {
+
+        try {
+            
+            await registerNewUser(req.body);
+
+            res.send('User Registerd Successfuly');
+        }
+        catch (err) {
+            res.send(err);
+         }
+    },
+
+    async loginUser(req, res) {
+
+        try {
+            
+            const token = await loginUser(req.body);
+            res.send(token);
+        }
+        catch (err) {
+            res.send(err);
+         }
+    },
+
+    async getUserProfile(req, res) {
+        try {
+            
+            const user = await getUserProfile(req.user.id);
+            res.send(user);
+        }
+        catch (err) {
+            res.send(err);
+         }
+    },
+
+    async updateUserProfile(req, res) {
+
+        try {
+            
+             await updateUserProfile(req.user.id , req.body);
+            res.send('User Updated Successfully');
+        }
+        catch (err) {
+            res.send(err);
+         }
+
+    },
 
 };
 
